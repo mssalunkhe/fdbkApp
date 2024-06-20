0a8c264d3a561adb65458e2029892582bad9fe88
Mahesh Salunkhe <mahesh.salunkhe@gmail.com>
2024-06-20 13:34:15 +0530

    first commit

---
+++ src/main/java/com/webapp/fdbkrestful/entity/Batch.java
@@ -0,0 +1,23 @@
+package com.webapp.fdbkrestful.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "batch")
+public class Batch {
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Column
+    String name;
+    @Column
+    String description;
+}
---
+++ src/test/java/com/webapp/fdbkrestful/FdbkRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.fdbkrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdbkRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
